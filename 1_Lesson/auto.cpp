52dd4d9f8bc06efdfd9babef9479f495f9b0c0a3
Vipon <valerakonychev@gmail.com>
2019-02-08 12:57:12 +0300

    Add comdef.h file for common macrosses.
    Update .travis.yml, Makefile and auto.cpp acordig to new comdef.h

--- 1_Lesson/auto.cpp
+++ 1_Lesson/auto.cpp
@@ -1,16 +1,21 @@
 #include <cstdio>
 #include <cstdint>
 
+#include "comdef.h"
+
+
 int main()
 {
     uint32_t a32 = -1;
     auto b32 = a32;
+    UNUSED(b32);
     printf("sizeof(a32) = %zu\n", sizeof(a32));
     printf("sizeof(b32) = %zu\n", sizeof(b32));
 
 
     uint64_t a64 = -1;
     auto b64 = a64;
+    UNUSED(b64);
     printf("sizeof(a64) = %zu\n", sizeof(a64));
     printf("sizeof(b64) = %zu\n", sizeof(b64));
 
---
+++ include/comdef.h
@@ -0,0 +1,6 @@
+#ifndef __COMDEF_H
+#define __COMDEF_H
+
+#define UNUSED(x) (void)(x);
+
+#endif /* __COMDEF_H */
