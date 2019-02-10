9218020345ee72aaa4facb83aeb5ab9448517ed5
LilVan <47319039+LilVan@users.noreply.github.com>
2019-02-10 15:16:39 +0300

    Update CPPMath_0.cpp

--- 1_Home_Work/CPPMath_0.cpp
+++ 1_Home_Work/CPPMath_0.cpp
@@ -30,17 +30,18 @@ auto Div(T0 &a, T1 &b)
 int main()
 {
     uint32_t a32 = 6, b32 = 4;
+
     printf("Sum(a32, b32) %d\n", Sum(a32, b32));
     printf("Sub(a32, b32) %d\n", Sub(a32, b32));
     printf("Mul(a32, b32) %d\n", Mul(a32, b32));
     printf("Div(a32, b32) %d\n", Div(a32, b32));
 
     double dVal = 2.0;
+    
     printf("Sum(a32, dVal) %lf\n", Sum(a32, dVal));
     printf("Sub(a32, dVal) %lf\n", Sub(a32, dVal));
     printf("Mul(a32, dVal) %lf\n", Mul(a32, dVal));
     printf("Div(a32, dVal) %lf\n", Div(a32, dVal));
 
-    getchar();
     return 0;
 }
