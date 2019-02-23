22b49ae4a708605d9a52e45dda738f869cf3eb37
LilVan <47319039+LilVan@users.noreply.github.com>
2019-02-23 11:35:17 +0300

    Update Stack.cpp

--- 2_Home_Work/Stack.cpp
+++ 2_Home_Work/Stack.cpp
@@ -1,22 +1,48 @@
-#include "Stack.h"
-#include <cstddef>
+#include "stack.h"
 #include <cstdio>
 #include <cstdlib> 
 #include <iostream>
 
+int error;
 
-Stack::Stack(size_t maxsize):size(maxsize) {
+Stack::Stack(size_t maxsize):size(maxsize), top(0) {
     data = new int[size];
-    top = 0;
+    if (data == nullptr) {
+        error = OUT_OF_MEMORY;
+    }
 }
 
-void Stack::push(int value) {
+
+int Stack::push(int value) {
+    if (top > value) {
+        int *temp = new int[size * 2];
+
+        if (temp == nullptr) {
+            error = OUT_OF_MEMORY;
+            return OUT_OF_MEMORY;
+        }
+
+        for (int i = 0; i < size; ++i) {
+            temp[i] = data[i];
+        }
+    
+        delete[] data;
+        data = temp;
+        return OK;
+    }
+
     data[top] = value;
     top++;
+    return OK;
 }
 
 
 int Stack::pop() {
+    if (top == 0) {
+        error = STACK_UNDERFLOW;
+        return STACK_UNDERFLOW;
+    }
+
     return data[--top];
 }
 
@@ -25,7 +51,7 @@ void Stack::dump() {
     size_t i;
     for (i = 0; i < top; i++) {
         std::cout << data[i];
-        std::cout << "    ";
+        std::cout << "   ";
     }
 }
 
