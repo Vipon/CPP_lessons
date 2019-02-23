3dc9185676cda56bf290fd8ddd2c8b62f8236fb7
LilVan <47319039+LilVan@users.noreply.github.com>
2019-02-23 11:35:21 +0300

    Update Stack.h

--- 2_Home_Work/Stack.h
+++ 2_Home_Work/Stack.h
@@ -1,7 +1,15 @@
 #ifndef STACK
 #define STACK
 
-#include <cstddef>
+#include <cstddef> 
+
+enum ERRORS {
+    OUT_OF_MEMORY = -103,
+    STACK_UNDERFLOW,
+    OK = 0
+};
+
+extern int error;
 
 class Stack {
 
@@ -10,7 +18,7 @@ public:
     ~Stack() {};
 
     void dump();
-    void push(int value);
+    int push(int value);
     int pop();
     int isFull();
     int isEmpty();
@@ -19,6 +27,6 @@ private:
     int *data;
     size_t top;
     const size_t size;
-};  
+};
 
 #endif
