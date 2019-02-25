a59ee013f6688fedf5c2f1cf1a189a6542deddc5
Eklmneur <47397429+Eklmneur@users.noreply.github.com>
2019-02-25 22:44:51 +0300

    Add files via upload

---
+++ 2_Home_Work/main.cpp
@@ -0,0 +1,30 @@
+#include "my_Stack.h"
+#include <iostream>
+#include <cstddef>
+
+int main()
+{
+    List <int> L;
+    for (std::size_t i = 0; i < 15; ++i){
+        L.push_back((int) i);
+    }
+
+    for (List<int>::iterator it = L.begin(); it != L.end(); ++it){
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
+
+    List<int>::iterator it = L[5];
+
+    for (std::size_t i = 0; i < 5; ++i){
+        it = L.remove(it);
+    }
+
+    for (List<int>::iterator it = L.begin(); it != L.end(); ++it){
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << L.front() << " " << L.back();
+    return 0;
+}
