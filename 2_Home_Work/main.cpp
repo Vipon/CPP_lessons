95e8b780f849d758c557826d1bbfdfa7dc7dea43
Eklmneur <47397429+Eklmneur@users.noreply.github.com>
2019-02-25 22:57:36 +0300

    Add files via upload

--- 2_Home_Work/main.cpp
+++ 2_Home_Work/main.cpp
@@ -6,7 +6,7 @@ int main()
 {
     List <int> L;
     for (std::size_t i = 0; i < 15; ++i){
-        L.push_back((static_cast<int>()) i);
+        L.push_back(static_cast<int>(i));
     }
 
     for (List<int>::iterator it = L.begin(); it != L.end(); ++it){
