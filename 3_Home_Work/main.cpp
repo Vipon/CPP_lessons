f42f979baf470f9c360587f169ffd161c04a068b
LilVan <47319039+LilVan@users.noreply.github.com>
2019-03-10 20:50:40 +0300

    Update main.cpp

--- 3_Home_Work/main.cpp
+++ 3_Home_Work/main.cpp
@@ -5,18 +5,19 @@
 
 int main()
 {
-    uint16_t crd1[9] = { 1, 2, 3,
-                         1, 2, 3,
-                         1, 2, 3 };
-    uint16_t crd2[9] = { 0, 0, 0,
+    uint16_t ex[9] = { 0, 0, 0,
                          1, 4, 6,
                          1, 3, 3 };
-    Mat a(static_cast<uint16_t*>(crd2), 9);
+    
+    Mat a(static_cast<uint16_t*>(ex), 9);
     Mat b(9);
 
     b = a;
     std::cout << b << '\n';
     std::cin >> b;
+    std::cout << b << '\n';
+    
+    b += a;
     std::cout << b << '\n';
 
     getchar(); getchar();
