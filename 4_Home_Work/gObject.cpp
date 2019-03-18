2b5015f0853ff9e29916fb6f74ead9a58c4b59ad
LilVan <47319039+LilVan@users.noreply.github.com>
2019-03-18 12:01:16 +0300

    Update gObject.cpp

--- 4_Home_Work/gObject.cpp
+++ 4_Home_Work/gObject.cpp
@@ -76,8 +76,8 @@ void Rectangle::draw(SDL_Renderer* renderer, int x, int y)
 
 void Regular_Polygon::draw(SDL_Renderer* renderer, int x, int y) {
     int v = 0;
-    int *px = new int[n+1]; //Массив для хранения точек x
-    int *py = new int[n+1]; //Массив для хранения точек y
+    double *px = new double[n+1]; //Массив для хранения точек x
+    double *py = new double[n+1]; //Массив для хранения точек y
   
     px[0] = x;
     py[0] = y;
