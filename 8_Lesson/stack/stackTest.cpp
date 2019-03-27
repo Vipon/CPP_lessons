8e56b87f018047fe35a71fb952ae24d6784b8ea2
Vipon <valerakonychev@gmail.com>
2019-03-27 14:49:06 +0300

    Add template static stack.

---
+++ 8_Lesson/stack/stackTest.cpp
@@ -0,0 +1,28 @@
+#include "stack.h"
+
+int main()
+{
+    Stack<> stDefault0;
+    Stack<> stDefault1(stDefault0);
+
+    // Default template arguments works as default function argument, so you
+    // can't specify last parameter without previous.
+    // Right
+    Stack<double> dStack;
+    Stack<long> lStack;
+    // Wrong
+    // Stack<10> defaultStack10;
+
+    Stack<double, 1> dStack1;
+    // Stack<double, 1> and Stack<double, 2000> are different classes, so we
+    // can't do the follow line.
+    // Wrong
+    // Stack<double, 2000> dStack2000(dStack1);
+
+    dStack.push(100);   // int -> double, because dStack have only method push(double&)
+    dStack.push(100.5);
+    lStack.push(100);
+    lStack.push(100.6); // float -> long, because lStack have only method push(long&)
+
+    return 0;
+}
