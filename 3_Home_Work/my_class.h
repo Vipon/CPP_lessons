4c495a91e660d4440b733caf60b2ffbcf8668e10
Yan <47361238+Yandeks@users.noreply.github.com>
2019-03-30 23:09:31 +0300

    Update my_class.h

--- 3_Home_Work/my_class.h
+++ 3_Home_Work/my_class.h
@@ -16,7 +16,7 @@ public:
 	Zp operator^(int pow);
 	int val = 0;
 	Zp& operator=(int lal);
-	friend std::ostream& operator<<(std::ostream& op, Zp& output);
+	friend std::ostream& operator<<(std::ostream& op, const Zp& output);
     	friend std::istream& operator>>(std::istream& in, Zp& input);
 private:
 	size_t Char = p; 
