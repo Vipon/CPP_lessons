2a043c27a9e1f5e14e5ed04e68fe655603b1b3de
Yandeks <47361238+Yandeks@users.noreply.github.com>
2019-04-02 21:37:37 +0300

    pract

---
+++ Babich_8_home_work/List.h
@@ -0,0 +1,87 @@
+#ifndef LIST_H
+#define LIST_H
+#include <iostream>
+
+template <typename T>
+struct Line {
+	T data;
+	struct Line<T>* next;
+};
+
+template <typename T>
+class List
+{
+public:
+	List(T firstline)
+	{
+		this->enter = new struct Line<T>;
+		this->enter->data = firstline;
+		this->enter->next = nullptr;
+		this->length++;
+	}
+	~List()
+	{
+		struct Line<T>* buf1 = this->enter;
+		struct Line<T>* buf2 = this->enter;
+		while (buf1->next != nullptr)
+		{
+			buf2 = buf1->next;
+			delete[] buf1;
+			buf1 = buf2;
+		}
+		delete[] buf1;
+	}
+	friend std::istream& operator>>(std::istream& stream, List<T>& input)
+	{
+		struct Line<T>* newLine = new struct Line<T>;
+		stream >> newLine->data;
+		newLine->next = nullptr;
+		struct Line<T>* buf = input.enter;
+		while (buf->next != nullptr)
+		{
+			buf = buf->next;
+		}
+		buf->next = newLine;
+		input.length++;
+		return stream;
+	}
+
+	friend std::ostream& operator<<(std::ostream& stream, List<T>& output)
+	{
+		struct Line<T>* buf = output.enter;
+		for (size_t i = 1; i < output.length+1; i++)
+		{
+			stream << buf->data << ' ';
+			buf = buf->next;
+		}
+		return stream;
+	}
+
+	void DelLine(struct Line<T>* prev)
+	{
+		struct Line<T>* buf = prev->next;
+		prev->next = prev->next->next;
+		delete[] buf;
+		if (this->length > 1)
+		this->length--;
+		return;
+	}
+
+	struct Line<T>* findline(T data)
+	{
+		struct Line<T>* buf = this->enter;
+		while (buf->next != nullptr)
+		{
+			if (buf->data == data)
+				return buf;
+			buf = buf->next;
+		}
+		return nullptr;
+	}
+
+private:
+	size_t length = 0;
+	struct Line<T>* enter;
+};
+
+#endif /*LIST_H*/
---
+++ Babich_8_home_work/List_main.cpp
@@ -0,0 +1,12 @@
+#include "List.h" 
+
+int main()
+{
+	List<int> ls(5);
+	std::cin >> ls >> ls >>ls;
+	std::cout << ls;
+	struct Line<int>* ptr = ls.findline(3);
+	ls.DelLine(ptr);
+	std::cout << ls;
+//	system("pause");
+}
