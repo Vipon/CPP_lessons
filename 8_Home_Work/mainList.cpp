914c3d2238a74f43dc668984df4d5f9af596b2b1
Eklmneur <47397429+Eklmneur@users.noreply.github.com>
2019-04-09 17:46:21 +0300

    Add files via upload

---
+++ 8_Home_Work/mainList.cpp
@@ -0,0 +1,35 @@
+#include "List.h"
+#include <cstddef>
+#include <iostream>
+
+int main()
+{
+    List <int> L;
+    for (std::size_t i = 0; i < 15; ++i){
+        L.push_back(static_cast<int>(i));
+    }
+
+    for (List<int>::iterator it = L.begin(); it != L.end(); ++it){
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
+
+    List<int>::iterator it = L[5];
+
+    for (std::size_t i = 0; i < 5; ++i){
+        it = L.remove(it);
+    }
+
+    for (List<int>::iterator it = L.begin(); it != L.end(); ++it){
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << L.front() << " " << L.back() << std::endl;
+    List <int> L2;
+    L2 = L;
+    std::cout << L2;
+    std::cin >> L2;
+    std::cout << L2;
+    return 0;
+}
