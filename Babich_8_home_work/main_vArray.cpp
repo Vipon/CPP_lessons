a1e5fb826bc724e3596dc33f29981ced749878d9
Yan <47361238+Yandeks@users.noreply.github.com>
2019-04-11 17:43:38 +0300

    Update main_vArray.cpp

--- Babich_8_home_work/main_vArray.cpp
+++ Babich_8_home_work/main_vArray.cpp
@@ -20,14 +20,13 @@ int main()
 	arr += r2d2;
 	arr.revert();
 	std::cout << arr;
-	arr[2] = 7;
-	std::cout << arr;
+	try{arr[2] = 7;}
+	catch (const class ArrayException& e)
+	{
+		std::cout << e.what();
+		return -1;
+	}
 	arr.sortUp();
 	std::cout << arr;
-	int& a = arr.findElem(2);
-	int& b = arr.findElem(7);
-	vArray<int> arr2(arr, b, a);
-	std::cout << arr2;
-
 //	system("pause");
 }
