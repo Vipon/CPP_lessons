dbe023b5d64638d2f7010585daa6351df45f2e2c
Alexandra Osicheva <osicheva.aiu@phystech.edu>
2019-04-15 16:40:04 +0300

    Edit files for 8 home work

--- 8_Home_Work/testArray.cpp
+++ 8_Home_Work/testArray.cpp
@@ -2,10 +2,16 @@
 
 int main() {
 	int arr[5] = {1, 2, 3, 4, 5};
-	Array<int, 5> iArr(arr);
-	std::cout << iArr;
-	std::cin >> iArr;
-	iArr.sort(0, 4);
-	std::cout << iArr;
+	try {
+		Array<int, 5> iArr(arr);
+		std::cout << iArr;
+		std::cin >> iArr;
+		iArr.sort(0, 4);
+		std::cout << iArr;
+	}
+	catch (std::exception& er) {
+		std::cerr << er.what();
+		exit(1);
+	}
 	return 0;
 }
