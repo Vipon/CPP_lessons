671a28940d47c48cee88fc5b76dbd9dc4563a1de
Eklmneur <47397429+Eklmneur@users.noreply.github.com>
2019-04-29 22:42:11 +0300

    Update Arraymain.cpp

--- 9_10_Home_Work/Arraymain.cpp
+++ 9_10_Home_Work/Arraymain.cpp
@@ -4,14 +4,19 @@
 
 int main()
 {
-    Array <int, 5> A;
-    std::cin >> A;
-    std::cout << A;
-    std::reverse(A.begin(), A.end());
-    std::cout << A << std::endl;
-    std::sort(A.begin(), A.end());
-    std::cout << A << std::endl;
-    std::iter_swap(A.begin(), --A.end());
-    std::cout << A << std::endl;
+    try{
+        Array <int, 5> A;
+        std::cin >> A;
+        std::cout << A;
+        std::reverse(A.begin(), A.end());
+        std::cout << A << std::endl;
+        std::sort(A.begin(), A.end());
+        std::cout << A << std::endl;
+        std::iter_swap(A.begin(), --A.end());
+        std::cout << A << std::endl;
+    }
+    catch (char *str){
+        return 1;
+    }
     return 0;
 }
