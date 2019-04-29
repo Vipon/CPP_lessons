65b5552c0825b2a9ac0b1df53d9425cca59bb478
Eklmneur <47397429+Eklmneur@users.noreply.github.com>
2019-04-29 22:43:43 +0300

    Update Varraymain.cpp

--- 9_10_Home_Work/Varraymain.cpp
+++ 9_10_Home_Work/Varraymain.cpp
@@ -4,29 +4,34 @@
 
 int main()
 {
-    Varray <int> A(5);
-    std::cin >> A;
-    std::cout << A;
-    std::cin >> *A[3];
-    auto it = A.begin();
-    for (int i = 0; i < 3; ++i){
-        ++it;
+    try{
+        Varray <int> A(5);
+        std::cin >> A;
+        std::cout << A;
+        std::cin >> *A[3];
+        auto it = A.begin();
+        for (int i = 0; i < 3; ++i){
+            ++it;
+        }
+        std::cout << *it << std::endl;
+        A.relength(10);
+        *A[5] = 10;
+        *A[6] = 223;
+        *A[7] = 8282;
+        *A[8] = 8;
+        *A[9] = 93;
+        std::cout << A;
+        A.relength(7);
+        std::cout << A << std::endl;
+        std::reverse(A.begin(), A.end());
+        std::cout << A << std::endl;
+        std::sort(A.begin(), A.end());
+        std::cout << A << std::endl;
+        std::iter_swap(A.begin(), --A.end());
+        std::cout << A << std::endl;
     }
-    std::cout << *it << std::endl;
-    A.relength(10);
-    *A[5] = 10;
-    *A[6] = 223;
-    *A[7] = 8282;
-    *A[8] = 8;
-    *A[9] = 93;
-    std::cout << A;
-    A.relength(7);
-    std::cout << A << std::endl;
-    std::reverse(A.begin(), A.end());
-    std::cout << A << std::endl;
-    std::sort(A.begin(), A.end());
-    std::cout << A << std::endl;
-    std::iter_swap(A.begin(), --A.end());
-    std::cout << A << std::endl;
+    catch(char *str){
+        return 1;
+    }
     return 0;
 }
