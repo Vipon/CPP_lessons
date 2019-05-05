556a0bc231b227d9b6ee20a778f4ce09f3c79592
LilVan <47319039+LilVan@users.noreply.github.com>
2019-05-05 18:18:59 +0300

    not final version

---
+++ 8_Home_Work/List.h
@@ -0,0 +1,161 @@
+#ifndef __LIST_H
+#define __LIST_H
+
+#include <cstddef>
+#include <cstring>
+#include <iostream>
+#include <exception>
+
+
+class ArrException : public std::exception {
+public:
+    ArrException(std::string&& err) noexcept : exc(std::move(err)) {}
+    ArrException(ArrException&& gExc) noexcept : exc(std::move(gExc.exc)) {}
+    const char* what() const noexcept { return exc.c_str(); }
+private:
+    std::string exc;
+};
+
+template <typename T>
+class List {
+
+public:
+    // default constructor
+    List() {};
+
+    List(T* Arr, size_t sizeArr) {
+        begin = new Elem;
+        Elem *current = begin;
+        for (int i = 0; i < sizeArr - 1; i++) {
+            current->val = Arr[i];
+            current->next = new Elem;
+            current = current->next;
+        }
+
+        current->val = Arr[sizeArr - 1];
+        current->next = nullptr;
+        size = sizeArr;
+     };
+
+    //copy constructor
+    List(const List<T>& L) {
+        Elem* current = L.begin;
+        begin = new Elem;
+        Elem* elem = begin;
+        elem->val = current->val;
+
+        while (current->next) {
+            elem->next = new Elem;
+            elem = elem->next; 
+            current = current->next;
+            elem->val = current->val;
+        }
+
+        elem->next = nullptr;
+        size = L.size;
+    };
+
+
+     //destructor
+    ~List() {
+        Elem* entry = begin;
+
+        while (entry) {
+            Elem* temp = entry;
+            entry = entry->next;
+            delete temp;
+        }
+        delete entry;
+    }
+
+    friend std::ostream& operator<<(std::ostream& out, List& list) {
+        Elem* current = list.begin;
+
+        while(current) {
+            out << current->val << "  ";
+            current = current->next;
+        };
+        out << std::endl;
+        return out;
+    }
+
+    //this fuction inserts element 'value' after element 'elem'
+    void insert(T value, T elem) {
+        Elem* current = begin;
+        while (current->val != elem && current) {
+            current = current->next;
+        }
+
+        if (current == nullptr) {
+            //element not find
+            //realize with throw?
+        }
+
+        Elem* addElem = new Elem;
+        Elem* temp = current->next;
+        current->next = addElem;
+        addElem->val = value;
+        addElem->next = temp;
+
+        size = size + 1;
+    }
+ 
+    //this function searches and removes first met element with value 'value'
+    void search_and_remove(T value) {
+        if (begin->val == value) {
+            Elem* temp = begin;
+            begin = begin->next;
+            delete temp;
+            size = size - 1;
+            return;
+        }
+
+        Elem* current = begin;
+        Elem* temp = current;
+
+        while (current) {
+            if (current->val == value) {
+                temp->next = current->next;
+                delete current;
+                size = size - 1;
+                return;
+            }
+            temp = current;
+            current = current->next;
+        }
+        return;
+    }
+
+
+    friend std::istream& operator>>(std::istream& is, List& list) {
+        T closing_el;
+        T val;
+        //first element you write wil be used only as closing one and not be put in list
+        is >> closing_el;
+        while(val != closing_el)
+        return is;
+    }
+    
+
+
+    T& operator[](size_t pos) {
+        Elem* b = begin;
+
+        for (size_t i = 0; i < pos; i++) {
+            b = b->next;
+        }
+        return b->val;
+    }
+    
+
+private:
+    struct Elem {
+        Elem *next;
+        T val;
+    };
+
+    Elem *begin;
+    size_t size;
+    };
+
+#endif //__LIST_H
---
+++ 8_Home_Work/testList.cpp
@@ -0,0 +1,12 @@
+#include "List.h"
+
+int main() {
+    
+    char Arr[3] = { 'd', 'i', 'k' };
+    List<char> list(Arr, 3);
+    List<char> list2(list);
+    list2.insert('s','i');
+    std::cout << list2;
+    list2.search_and_remove('k');
+    std::cout << list2;
+}
