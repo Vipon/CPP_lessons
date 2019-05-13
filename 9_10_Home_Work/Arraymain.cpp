7a4572830113946a32be67dc53c6410ab82c4766
Eklmneur <47397429+Eklmneur@users.noreply.github.com>
2019-05-13 20:07:21 +0300

    Add files via upload

--- 9_10_Home_Work/Arraymain.cpp
+++ 9_10_Home_Work/Arraymain.cpp
@@ -4,19 +4,14 @@
 
 int main()
 {
-    try{
-        Array <int, 5> A;
-        std::cin >> A;
-        std::cout << A;
-        std::reverse(A.begin(), A.end());
-        std::cout << A << std::endl;
-        std::sort(A.begin(), A.end());
-        std::cout << A << std::endl;
-        std::iter_swap(A.begin(), --A.end());
-        std::cout << A << std::endl;
-    }
-    catch (char *str){
-        return 1;
-    }
+    Array <int, 5> A;
+    std::cin >> A;
+    std::cout << A;
+    std::reverse(A.begin(), A.begin() + A.NUM);
+    std::cout << A << std::endl;
+    std::sort(A.begin(), A.begin() + A.NUM);
+    std::cout << A << std::endl;
+    std::iter_swap(A.begin(), A.begin() + A.NUM - 1);
+    std::cout << A << std::endl;
     return 0;
 }
--- 9_10_Home_Work/Varraymain.cpp
+++ 9_10_Home_Work/Varraymain.cpp
@@ -2,40 +2,26 @@
 #include <algorithm>
 #include <iostream>
 
-void poshel_daleko_etot_stat_analizator(){
-    try{
-        Varray <int> A(5);
-        std::cin >> A;
-        std::cout << A;
-        std::cin >> *A[3];
-        auto it = A.begin();
-        for (int i = 0; i < 3; ++i){
-            ++it;
-        }
-        std::cout << *it << std::endl;
-        A.relength(10);
-        *A[5] = 10;
-        *A[6] = 223;
-        *A[7] = 8282;
-        *A[8] = 8;
-        *A[9] = 93;
-        std::cout << A;
-        A.relength(7);
-        std::cout << A << std::endl;
-        std::reverse(A.begin(), A.end());
-        std::cout << A << std::endl;
-        std::sort(A.begin(), A.end());
-        std::cout << A << std::endl;
-        std::iter_swap(A.begin(), --A.end());
-        std::cout << A << std::endl;
-    }
-    catch(char *str){
-        return;
-    }
-}
-
 int main()
 {
-    poshel_daleko_etot_stat_analizator();
+    Varray <int> A(5);
+    std::cin >> A;
+    std::cout << A;
+    A.relength(10);
+    A.NUM = 10;
+    A[5] = 10;
+    A[6] = 223;
+    A[7] = 8282;
+    A[8] = 8;
+    A[9] = 93;
+    std::cout << A;
+    A.relength(7);
+    std::cout << A << std::endl;
+    std::reverse(A.begin(), A.end());
+    std::cout << A << std::endl;
+    std::sort(A.begin(), A.end());
+    std::cout << A << std::endl;
+    std::iter_swap(A.begin(), --A.end());
+    std::cout << A << std::endl;
     return 0;
 }
