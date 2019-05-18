654aa332dcfb6237d001f635dd48d009bdd00092
Yan <47361238+Yandeks@users.noreply.github.com>
2019-05-18 16:14:28 +0300

    Update rectangle.cpp

--- Babich_4_home_work/rectangle.cpp
+++ Babich_4_home_work/rectangle.cpp
@@ -2,6 +2,6 @@
 
 void Rectangle::draw(SDL_Renderer* rend, int x, int y)
 {
-	SDL_Rect rectangle = { x, y, sideline1, sideline2 }
+	SDL_Rect rectangle = { x, y, sideline1, sideline2 };
 	SDL_RenderDrawRect(rend, &rectangle);
 }
