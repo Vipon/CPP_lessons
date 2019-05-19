2e39c90ebcfcce332ba0d7b517f11dff797581ba
SenchurovaAV <47359985+SenchurovaAV@users.noreply.github.com>
2019-05-19 14:09:18 +0300

    upload Stack.h

---
+++ 2_Home_Work/Stack.h
@@ -0,0 +1,29 @@
+#ifndef __STACK_H
+#define __STACK_H
+// #include <iostream>
+#include <cstdint>
+#include <cstddef>
+
+class Stack
+{
+public:
+	
+	Stack(size_t size = 9);
+	~Stack();
+
+	void push(uint32_t val);
+	uint32_t pop();
+
+	int isFull();
+	int isEmpty();
+	void dump();
+
+private:
+	
+	uint32_t *stack;
+	size_t size;
+	size_t cur;
+
+};
+
+#endif // STACK_H
