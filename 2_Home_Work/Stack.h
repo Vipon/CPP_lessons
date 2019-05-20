4e819afdcd48eea198572028b435d01c6f08de2c
SenchurovaAV <47359985+SenchurovaAV@users.noreply.github.com>
2019-05-20 06:02:57 +0300

    Header, please

--- 2_Home_Work/Stack.h
+++ 2_Home_Work/Stack.h
@@ -1,29 +1,26 @@
 #ifndef __STACK_H
 #define __STACK_H
-// #include <iostream>
+
 #include <cstdint>
+#include <cstddef>
 
+class Stack {
 
-class Stack
-{
 public:
-	
-	Stack(size_t size = 9);
+	Stack (size_t size = 9);
 	~Stack();
-
-	void push(uint32_t val);
-	uint32_t pop();
-
-	bool isFull();
-	bool isEmpty();
+	void push (uint32_t dat);
+	uint64_t pop ();
 	void dump();
+	bool isEmpty();
+	bool isFull();
+
 
 private:
-	
 	uint32_t *stack;
 	size_t size;
 	size_t cur;
 
 };
 
-#endif // __STACK_H
+#endif //__STACK_H 
