342989fcca849fe70c68d9d17bbd26b0ca1747b2
SenchurovaAV <47359985+SenchurovaAV@users.noreply.github.com>
2019-05-20 06:09:25 +0300

    Maybe...

--- 2_Home_Work/main.cpp
+++ 2_Home_Work/main.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
 #include "Stack.h"
+#include <iostream>
 
 int main () 
 {
@@ -13,8 +13,11 @@ int main ()
 	S1.push(42);
 	S1.push(26);
 	S1.dump(); //printout
-	std::cout << S1.push(19) << std::endl;
+	uint32_t IP = S1.pop();
+	std::cout << IP << std::endl;
 
 	return 0; // fantastic
 
 }
+
+	
